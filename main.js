57e2100bd2582be68cfcedd4ad26ec49926e1dbb
BuildTools <unconfigured@null.spigotmc.org>
2020-02-10 18:42:38 +0100

    Add image resizing function

--- main.js
+++ main.js
@@ -6,24 +6,88 @@ const bottomCaptionInput = document.querySelector('#bottomCaption');
 const imageInput = document.querySelector('#imageFile');
 const downloadButton = document.querySelector('#download');
 
+canvas.height = 300;
+canvas.width = 300;
+
+canvas.style.width = 300;
+canvas.style.height = 300;
+
 let meme = {
-    width: canvas.height,
-    height: canvas.width,
+    width: canvas.width,
+    height: canvas.height,
     topCaption: '', 
     bottomCaption: '',
 };
 
 let image = new Image();
 
+const textWrap = (text, isTop) => {
+    const lineHeight = 30;
+    const maxLineWidth = canvas.width - 20;
+
+    if(isTop){
+        yPos = 25;
+    } else{
+        yPos = meme.height - 10;
+    }
+
+    let words = text.slice(' ');
+    let line = '';
+    let n = 0;
+
+    for(let i = 0; i < words.length; ++i){
+
+        line += words[i];
+
+        const lineWidth = context.measureText(line).width - n*meme.width;
+
+        if(lineWidth > maxLineWidth){
+            line += 'space';
+            n += 1;
+        };
+    }
+
+    let lines = line.split('space');
+
+    lines = isTop ? lines : lines.reverse();
+
+    lines.forEach(textLine => {
+
+        context.fillStyle = "rgba(0, 0, 0, 0.5)";
+        context.strokeText(textLine, meme.width/2, yPos);
+
+        context.fillStyle = "rgba(255, 255, 255, 1.0)";
+        context.fillText(textLine, meme.width/2, yPos);
+
+        isTop ? yPos += lineHeight : yPos -= lineHeight;
+    
+    });
+}
+
 const downloadImage = () => {
     downloadButton.href = canvas.toDataURL("image/jpeg");
 };
 
-const canvasImageRender = () => {
+const resizeImage = (height, width) => {
+    let maxCanvasSize = 300;
+    const zoom = Math.max(height, width) / maxCanvasSize;
 
-    image.addEventListener("load", () => {
-        context.drawImage(image, 0, 0, meme.width, meme.height);
-    }, false);
+    if(height > width){
+        height = maxCanvasSize;
+        width /= zoom;
+    } else{
+        width = maxCanvasSize;
+        height /= zoom;
+    };
+
+    return {
+        x: width,
+        y: height
+    };
+};
+
+const canvasImageRender = () => {
+    image = new Image();
 
     if(imageInput.files && imageInput.files[0]){
         let reader = new FileReader();
@@ -35,38 +99,49 @@ const canvasImageRender = () => {
         reader.readAsDataURL(imageInput.files[0]);
     }
 
-    image.src = meme.src;
+    image.addEventListener("load", e => {
+        const currentImage = e.target;
+
+        const size = resizeImage(currentImage.height, currentImage.width);
+
+        currentImage.width = size.x;
+        currentImage.height = size.y;
+
+        meme.width = size.x;
+        meme.height = size.y;
+    
+        canvas.height = size.y;
+        canvas.width = size.x;
+
+        /*canvas.style.width = meme.width;
+        canvas.style.height = meme.height;*/
+
+        context.drawImage(currentImage, 0, 0, meme.width, meme.height);
+    }, false);
 
     canvasTextRender();
 };
 
 const canvasTextRender = () => {
-    context.clearRect(0, 0, meme.width, meme.height);
+    const fontSize = 20;
 
+    context.clearRect(0, 0, meme.width, meme.height);
     context.drawImage(image, 0, 0, meme.width, meme.height);
 
     meme.topCaption = topCaptionInput.value;
     meme.bottomCaption = bottomCaptionInput.value;
 
-    context.font = "20px Anton";
+    context.font = `${fontSize}px Anton`;
     context.textAlign = "center";
-    context.lineWidth = 3;
+    context.lineWidth = 5;
 
-    context.fillStyle = "rgba(0, 0, 0, 0.5)";
-    context.strokeText(meme.topCaption, meme.width/2, 25);
-
-    context.fillStyle = "rgba(255, 255, 255, 1.0)";
-    context.fillText(meme.topCaption, meme.width/2, 25);
-
-    context.fillStyle = "rgba(0, 0, 0, 0.5)";
-    context.strokeText(meme.bottomCaption, meme.width/2, meme.height - 10);
-
-    context.fillStyle = "rgba(255, 255, 255, 1.0)";
-    context.fillText(meme.bottomCaption, meme.width/2, meme.height - 10);
+    textWrap(meme.topCaption, true);
+    textWrap(meme.bottomCaption, false);
 };
 
 topCaptionInput.addEventListener("keyup", canvasTextRender, false);
 bottomCaptionInput.addEventListener("keyup", canvasTextRender, false);
 imageInput.addEventListener("change", canvasImageRender, false);
+imageInput.addEventListener("change", canvasTextRender, false);
 
 downloadButton.addEventListener("click", downloadImage, false);
